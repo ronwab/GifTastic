3faaaf7bb200e5ff854503472b4c9d00b3838892
ronaldwabukenda <ronald.wabukenda@salesloft.com>
2019-04-06 17:15:01 -0400

    more css updates

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -2,27 +2,40 @@ $(document).ready(function () {
     var displayimages
     var searchVariable
     var testings
-    var sports = ['running', 'football', 'soccer', 'baseball', "hockey"]
+    var sports = ['UGA', 'Georgia State', 'Georgia Southern', 'Penn State', "Florida State", "Auburn", "Alabama", "Missispi state"]
+    var inputText
+    var data
+    // var numberofRowsButtons = $("<tr>").va
+    // var tableBody = ("<table>")
+
+    // function createButtonTable() {
+    //     var newtable = $("<input>").attr({
+    //         type: "table",
+    //         id: "newTable"
+    //     })
+
+    //     $("testTable").append(newtable)
+    // }
+
+    // createButtonTable()
+
     for (i = 0; i < sports.length; i++) {
 
-        //creates buttons from array
         searchVariable = sports[i]
-        // console.log(searchVariable);
-        //creates a new button for each object in the array
+
         var createButtons = $('<input/>').attr({
             type: 'button',
             id: searchVariable,
             value: searchVariable,
-            class: "button",
+            class: "button button-style  btn btn-primary"
         });
 
 
         $("#sportsdiv").append(createButtons)
     }
 
-
     $("#submit").on("click", function () {
-        var inputText = $('#searchTextBox').val().trim()
+        inputText = $('#searchTextBox').val().trim()
         console.log(inputText);
 
         sports.push(inputText)
@@ -34,22 +47,27 @@ $(document).ready(function () {
         })
 
         // creates new buttons
+        createDynamicButton()
+
+    })
+
+    function createDynamicButton() {
         var createButtons = $('<input/>').attr({
             type: 'button',
             id: inputText,
             value: inputText,
-            class: "button"
+            class: "button button-style  btn btn-primary"
         });
         $("#sportsdiv").append(createButtons)
         $('#searchTextBox').val(" ")
 
-    })
+    }
     $(document).on("click", ".button", function () {
 
         var test2 = $(this).attr("id")
 
         giphyurl = "https://api.giphy.com/v1/gifs/search?q=" + test2 + "&api_key=dc6zaTOxFJmzC&limit=10&rating=g";
-        // giphyurl = " https://api.giphy.com/v1/gifs/search?api_key=WnI8FnWU9ICjRJwARWWZVGx415L5pgFO&q=" + searchVariable + "&limit=10&rating=g"
+        // giphyurl = " https://api.giphy.com/v1/gifs/search?api_key=WnI8FnWU9ICjRJwARWWZVGx415L5pgFO&q=" + searchVariable + "&limit=10"
 
         $.ajax({
             url: giphyurl,
@@ -58,18 +76,13 @@ $(document).ready(function () {
 
                 for (j = 0; j <= 10; j++) {
 
-
                     var image = '<img src= " ' + response.data[j].images.fixed_height_still.url +
                         '" data-still=" ' + response.data[j].images.fixed_height_still.url +
                         ' " data-animate=" ' + response.data[j].images.fixed_height.url +
 
                         '" data-state="still" class="gifImage" style= "width:250px; height:250px">';
-                    // display the image
 
-
-
-
-                    image = '<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">' + image + "</div>";
+                    // image = '<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">' + image + "</div>";
                     $('#sportsdiv').append(image);
 
                 }
@@ -82,21 +95,25 @@ $(document).ready(function () {
 
         console.log(event);
 
-        var data = event.target.dataset
+        data = event.target.dataset
 
+        flipImage()
+    })
+
+    function flipImage() {
         var state = data.state;
         // to run the gif
         if (state == 'still') {
-
-
             event.target.src = data.animate
             data.state = "animate"
         } else {
             // To stop the gif
-
             event.target.src = data.still
             data.state = "still"
         };
-    })
+    }
 
+    $(document).on("click", ".reset", function () {
+        window.location.reload();
+    })
 })
